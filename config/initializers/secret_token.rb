8d5d305da5b4a7594711379bd99a5bff6d821b15
Emre Tanirgan <emretanirgan@hotmail.com>
2013-01-11 11:10:56 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railslolcatsapp::Application.config.secret_token = '6bb8c56c7494b171e56dd127abfb2e4f79ff9acd03e6c338ff6c44ca11d910bca953fe68130ae1e4d1bcee78795fe9fab378cf8c1f0be468c6ab63737595976b'
